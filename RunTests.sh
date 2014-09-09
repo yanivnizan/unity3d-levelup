342663e89ee7b53a98925cb42d911e351b554634
Dima <dimanemets@gmail.com>
2014-09-09 16:15:44 +0300

    Script changed

--- RunTests.sh
+++ RunTests.sh
@@ -2,4 +2,8 @@
 # Command Structure: UNITY_EXECUTABLE_PATH -batchmode -projectPath PATH_TO_YOUR_PROJECT -executeMethod UnityTest.Batch.RunUnitTests -resultFilePath=PATH_TO_RES_FILE
 # By default, the following output file will be created: ./Soomla/UnitTestResults.xml
 
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -executeMethod UnityTest.Batch.RunUnitTests 
+echo "Executing unit tests"
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-executeMethod UnityTest.Batch.RunUnitTests \ 
